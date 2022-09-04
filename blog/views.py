33222604639ee631ffecb8dc3ae4df08ac71a7bd
Nitesh Thapliyal <niteshnits.thapliyal@gmail.com>
2022-09-04 23:33:46 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'NiteshSBlog',
-        'title': 'Blog Post 1',
-        'comment': 'First post content',
-        'date_posted': 'August_21'
-    },
-
-       {
-        'author': 'BGSBlog',
-        'title': 'Blog Post 2',
-        'comment': 'Second post content',
-        'date_posted': 'August_28'
-    },
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
