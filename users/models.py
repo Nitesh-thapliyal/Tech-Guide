0bd2d8051e5658fd3abd4b302d4abb145f921248
Nitesh Thapliyal <niteshnits.thapliyal@gmail.com>
2022-09-11 00:38:41 +0530

    update

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from PIL import Image
+# Create your models here. 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,3 +10,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return  f'{self.user.username}Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
